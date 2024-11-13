ff456d19138c97465498429c66de19e6b4aa70fa
vinlytolentino <vgtolentino1@up.edu.ph>
2024-11-13 22:47:27 +0800

    Initial Commit

--- main.py
+++ main.py
@@ -16,6 +16,38 @@ def lexemes_matcher ():
         elif (re.search("^KTHXBYE$", line) != None):
            token_dict["KTHXBYE"] = "Code Delimiter"
 
+        elif (re.search("^WAZZUP$", line) != None):
+           token_dict["WAZZUP"] = "Code Delimiter"
+
+        elif (re.search("^BUHBYE$", line) != None):
+           token_dict["BUHBYE"] = "Code Delimiter"
+        
+        elif (re.search("^BTW$", line) != None):
+           token_dict["BTW"] = "Code Delimiter"
+
+        elif (re.search("^OBTW$", line) != None):
+           token_dict["OBTW"] = "Code Delimiter"
+
+        elif (re.search("^TLDR$", line) != None):
+           token_dict["TLDR"] = "Code Delimiter"
+
+        elif (re.search("^I HAS A$", line) != None):
+           token_dict["I HAS A"] = "Code Delimiter"
+
+        elif (re.search("^ITZ$", line) != None):
+           token_dict["ITZ"] = "Code Delimiter"
+
+        elif (re.search("^R$", line) != None):
+           token_dict["R"] = "Code Delimiter"
+        
+        elif (re.search("^SUM OF$", line) != None):
+           token_dict["SUM OF"] = "Code Delimiter"
+
+        elif (re.search("^DIFF OF$", line) != None):
+           token_dict["DIFF OF"] = "Code Delimiter"
+
+      
+
     print(token_dict)
 
 lexemes_matcher()
