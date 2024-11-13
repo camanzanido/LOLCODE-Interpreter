9ee6f0b22be5b2ff7fafbee3fbc353cffb689e09
vinlytolentino <vgtolentino1@up.edu.ph>
2024-11-14 00:47:34 +0800

    refactor: exhaust line to match possible regex

--- main.py
+++ main.py
@@ -1,7 +1,77 @@
 import re
 
-def lexemes_matcher ():
-    
+def lexemes_matcher (line):
+    token_list = []
+
+    # If the line contains 'BTW' followed by any text, exit early
+    if re.search("^BTW .*$", line) != None:
+        return token_list
+
+    # Handle the case where 'BTW' is in the line
+    if 'BTW' in line:
+        line = line.split('BTW')[0].strip()
+
+    if re.search("^HAI$", line) != None:
+        token_list.append("HAI")
+        token_list.append("Code Delimiter")
+        
+    elif re.search("^KTHXBYE$", line) != None:
+        token_list.append("KTHXBYE")
+        token_list.append("Code Delimiter")
+
+    elif re.search("^WAZZUP$", line) != None:
+        token_list.append("WAZZUP")
+        token_list.append("Code Delimiter")
+
+    elif re.search("^BUHBYE$", line) != None:
+        token_list.append("BUHBYE")
+        token_list.append("Code Delimiter")
+        
+    elif re.search("^BTW$", line) != None:
+        token_list.append("BTW")
+        token_list.append("Code Delimiter")
+
+    elif re.search("^OBTW$", line) != None:
+        token_list.append("OBTW")
+        token_list.append("Code Delimiter")
+
+    elif re.search("^VISIBLE$", line) != None:
+        token_list.append("VISIBLE")
+        token_list.append("Code Delimiter")
+
+    elif re.search("^TLDR$", line) != None:
+        token_list.append("TLDR")
+        token_list.append("Code Delimiter")
+
+    elif re.search("^I HAS A ", line) != None:
+        token_list.append("I HAS A")
+        token_list.append("Code Delimiter")
+
+    elif re.search("^ITZ$", line) != None:
+        token_list.append("ITZ")
+        token_list.append("Code Delimiter")
+
+    elif re.search("^R$", line) != None:
+        token_list.append("R")
+        token_list.append("Code Delimiter")
+        
+    elif re.search("^SUM OF $", line) != None:
+        token_list.append("SUM OF")
+        token_list.append("Code Delimiter")
+        
+    elif re.search("^DIFF OF $", line) != None:
+        token_list.append("DIFF OF")
+        token_list.append("Code Delimiter")
+        
+    elif re.search("^[a-zA-Z][a-zA-Z0-9_]* ", line) != None:
+        variable = re.search("[a-zA-Z][a-zA-Z0-9_]* ", line).group()
+        token_list.append(variable)
+        token_list.append("Variable Identifier")
+    return token_list
+
+
+def lexemes_init():
+  
     file = open("input.txt", "r")
     lines = file.readlines()
 
@@ -10,44 +80,18 @@ def lexemes_matcher ():
     # Per line of the code
     for line in lines:
         
-        if (re.search("^HAI$", line) != None):
-           token_dict["HAI"] = "Code Delimiter"
-        
-        elif (re.search("^KTHXBYE$", line) != None):
-           token_dict["KTHXBYE"] = "Code Delimiter"
+        line = line.strip()
+        while line != "":
+            token = lexemes_matcher(line)
+            if token != []:
+                token_dict[token[0]] = token[1]
+                line = line.replace(token[0], "").strip()
+            else:
+                break
+    
+    for key, value in token_dict.items():
+        print(f"{key}: {value}")
 
-        elif (re.search("^WAZZUP$", line) != None):
-           token_dict["WAZZUP"] = "Code Delimiter"
 
-        elif (re.search("^BUHBYE$", line) != None):
-           token_dict["BUHBYE"] = "Code Delimiter"
-        
-        elif (re.search("^BTW$", line) != None):
-           token_dict["BTW"] = "Code Delimiter"
+lexemes_init()
 
-        elif (re.search("^OBTW$", line) != None):
-           token_dict["OBTW"] = "Code Delimiter"
-
-        elif (re.search("^TLDR$", line) != None):
-           token_dict["TLDR"] = "Code Delimiter"
-
-        elif (re.search("^I HAS A$", line) != None):
-           token_dict["I HAS A"] = "Code Delimiter"
-
-        elif (re.search("^ITZ$", line) != None):
-           token_dict["ITZ"] = "Code Delimiter"
-
-        elif (re.search("^R$", line) != None):
-           token_dict["R"] = "Code Delimiter"
-        
-        elif (re.search("^SUM OF$", line) != None):
-           token_dict["SUM OF"] = "Code Delimiter"
-
-        elif (re.search("^DIFF OF$", line) != None):
-           token_dict["DIFF OF"] = "Code Delimiter"
-
-      
-
-    print(token_dict)
-
-lexemes_matcher()
