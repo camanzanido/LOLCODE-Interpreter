cf14d09cfe00527303f9419fdbf715d98fac3ba6
vinlytolentino <vgtolentino1@up.edu.ph>
2024-11-13 22:39:16 +0800

    initial commit: Lexical Analyzer

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import re
 
-file = open("input.txt", "r")
-lines = file.readlines()
+def lexemes_matcher ():
+    
+    file = open("input.txt", "r")
+    lines = file.readlines()
 
-for line in lines:
-    print(line, end="")
+    token_dict = {}
+
+    # Per line of the code
+    for line in lines:
+        
+        if (re.search("^HAI$", line) != None):
+           token_dict["HAI"] = "Code Delimiter"
+        
+        elif (re.search("^KTHXBYE$", line) != None):
+           token_dict["KTHXBYE"] = "Code Delimiter"
+
+    print(token_dict)
+
+lexemes_matcher()
